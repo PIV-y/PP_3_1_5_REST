a0e4939cc77ba3d2c3c462a657e935f79bd8e254
PIV-y <iv.proshin@yandex.ru>
2023-09-28 11:17:12 +0400

    com7

--- src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/SuccessUserHandler.java
@@ -1,9 +1,12 @@
 package ru.kata.spring.boot_security.demo.configs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.services.UserDetailsServiceApp;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,16 +15,24 @@ import java.util.Set;
 
 @Component
 public class SuccessUserHandler implements AuthenticationSuccessHandler {
+    private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceApp.class);
+
     // Spring Security использует объект Authentication, пользователя авторизованной сессии.
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
         Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-        if (roles.contains("ROLE_ADMIN")) {
+        log.info("Получение ролей для аунтентификации...");
+//        if (roles.contains("ROLE_ADMIN")) {
+        if (roles.contains("ADMIN")) {
+            log.info("Админ, добро пожаловать...");
             httpServletResponse.sendRedirect("/admin/");
-        } else if (roles.contains("ROLE_USER")) {
+//        } else if (roles.contains("ROLE_USER")) {
+        } else if (roles.contains("USER")) {
+            log.info("User, добро пожаловать...");
             httpServletResponse.sendRedirect("/user");
         }
         else {
+            log.info("Роль не определена...");
             httpServletResponse.sendRedirect("/");
         }
     }
--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -3,13 +3,16 @@ package ru.kata.spring.boot_security.demo.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import ru.kata.spring.boot_security.demo.services.UserDetailsServiceApp;
 
 @Configuration
@@ -32,7 +35,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+//                .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
+                .antMatchers("/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER","ADMIN")
                 .and()
                 .formLogin()
@@ -43,7 +47,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .failureUrl("/auth/login?error")
                 .and()
                 .logout().logoutUrl("/logout")
-                .logoutSuccessUrl("/auth/login");
+//                .logoutSuccessUrl("/auth/login");
+                .logoutSuccessUrl("/login");
     }
 
     @Bean
@@ -58,5 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         authenticationProvider.setUserDetailsService(userDetailsServiceApp);
         return authenticationProvider;
     }
-
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+    }
 }
