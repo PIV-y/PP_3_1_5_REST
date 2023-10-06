a0fdc6fc9c365ab9b36e68488dbcc6b6c20ef7cb
PIV-y <iv.proshin@yandex.ru>
2023-10-06 09:51:21 +0400

    com11

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
@@ -13,7 +13,7 @@ import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
 import java.util.List;
 
-@Component
+@Service
 public class UserServiceImpl implements UserService{
 
     private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
